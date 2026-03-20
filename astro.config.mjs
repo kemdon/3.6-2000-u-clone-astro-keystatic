65bd58548d2524d7f4d4bc6288f5b4c414f42424
kemdon <nijinchao@gmail.com>
2026-03-20 16:54:29 +0800

    Use Vercel adapter for deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
-import node from '@astrojs/node';
 import react from '@astrojs/react';
+import vercel from '@astrojs/vercel';
 
 export default defineConfig({
   site: 'https://www.birthdaycakecandle.com',
@@ -14,9 +14,7 @@ export default defineConfig({
     port: 4321,
   },
   integrations: [react()],
-  adapter: node({
-    mode: 'standalone',
-  }),
+  adapter: vercel(),
   vite: {
     optimizeDeps: {
       entries: ['keystatic.config.js'],
